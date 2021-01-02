f2d232d195831ba027140313fccffef9669edb8a
Andre Luis <adrluis7@gmail.com>
2021-01-02 19:40:17 -0300

    Exercício 1 - Dividindo X por Y

---
+++ Introdução a Programação/Exercicio 1 - Dividindo X por Y/Dividindo_X_por_Y.js
@@ -0,0 +1,14 @@
+let limit = parseInt(gets());
+for (let i = 0; i < limit; i++) {
+let line = gets().split(" ");
+let X = parseInt(line[0]);
+let Y = parseInt(line[1]);
+if (Y == 0) {
+console.log("divisao impossivel");
+} else {
+let divisao = parseFloat(X / Y ).toFixed(1); // escreva seu código aqui
+
+
+console.log(divisao);
+}
+}
