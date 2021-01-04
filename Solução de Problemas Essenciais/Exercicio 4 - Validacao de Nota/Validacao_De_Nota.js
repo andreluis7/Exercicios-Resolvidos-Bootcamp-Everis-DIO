662ced6606ee191a55daa03e653e78e3bd1f74b6
Andre Luis <adrluis7@gmail.com>
2021-01-03 21:55:33 -0300

    Exercício 4 - Validação de Nota

---
+++ Solução de Problemas Essenciais/Exercicio 4 - Validacao de Nota/Validacao_De_Nota.js
@@ -0,0 +1,26 @@
+let qtdeNotasValidas = 0;
+
+let notas = [];
+
+let cont = 0;
+
+while(qtdeNotasValidas < 2) {
+  cont++;
+  
+  const nota = parseFloat(gets());
+  
+  if(!nota) break;
+  
+  if(nota >= 0 && nota <= 10) {
+    notas.push(nota)
+    qtdeNotasValidas++;
+  }else{
+    console.log('nota invalida')
+  }
+}
+
+const arraySum = arr => arr.reduce((acc, current) => acc + current)
+
+const arrayMedia = arr => (arraySum(arr) / arr.length).toFixed(2)
+
+console.log("media = " + arrayMedia(notas))
